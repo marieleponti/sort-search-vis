eb4b5fa4e1d9b49a9f5580dc04215609a97da5f0
Mariele Ponti <mpon3004@students.vsu.edu>
2021-04-07 10:04:07 -0400

    added methods

--- AVLTree.java
+++ AVLTree.java
@@ -1,2 +1,80 @@
-public class AVLTree <AnyType extends Comparable<? super AnyType>>{
+public class AVLTree<AnyType extends Comparable<? super AnyType>> extends  BinarySearchTree{
+    private static final int maxHeightDif = 1;
+    private static class Node<AnyType>{
+        AnyType data;
+        Node<AnyType> left;
+        Node<AnyType> right;
+        int height;
+
+        Node(AnyType x){
+            this(x, null, null, 0);
+        }
+
+        Node(AnyType x, Node<AnyType> lt, Node<AnyType> rt, int h){
+            data = x;
+            left = lt;
+            right = rt;
+            height = h;
+        }
+    }
+
+    private int height(Node<AnyType> t){
+        return t == null? -1 : t.height;
+    }
+
+    private Node<AnyType> insert(AnyType x, Node<AnyType> t){
+        super.insert(x);
+        return balance(t);
+    }
+
+    private Node<AnyType> balance(Node<AnyType> t){
+        if (t == null) return null;
+        if (height(t.left) - height(t.right) > maxHeightDif){
+            // left subtree's height needs to be reduced
+            /* check if left subtree's left side is taller than
+             * left subtree's right side
+             */
+            if (height(t.left.left) >= height(t.left.right)){
+                t = rotateWithLeftChild(t);
+            } else {
+                t = doubleWithLeftChild(t);
+            }
+        } else if (height(t.right) - height(t.left) > maxHeightDif){
+
+        }
+        return t;
+    }
+
+    private Node<AnyType> rotateWithLeftChild(Node<AnyType> k2){
+        Node<AnyType> k1 = k2.left;
+        k2.right = k1.left;
+        k1.left = k2;
+        k2.height = Math.max(height(k2.left), height(k2.right)) + 1;
+        k1.height = Math.max( k2.height, height(k2.right)) + 1;
+        return k1;
+    }
+
+    private Node<AnyType> rotateWithRightChild(Node<AnyType> k2){
+        Node<AnyType> k1 = k2.right;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max(height(k2.left), height(k2.right)) + 1;
+        k1.height = Math.max(height(k1.left), k2.height) + 1;
+        return k1;
+    }
+
+    private Node<AnyType> doubleWithLeftChild(Node<AnyType> k3){
+        k3.left = rotateWithRightChild(k3.left);
+        return rotateWithLeftChild(k3);
+    }
+
+    private Node<AnyType> doubleWithRightChild(Node<AnyType> k3){
+        k3.right = rotateWithLeftChild(k3.right);
+        return rotateWithRightChild(k3);
+    }
+
+    private Node<AnyType> remove(AnyType x, Node<AnyType> t){
+        super.remove(x);
+        return balance(t);
+    }
 }
